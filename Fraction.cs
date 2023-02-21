518cf372651dd23827090eae541a67cca78b88bc
Юрий <yshulga9@gmail.com>
2023-02-21 15:58:44 +0300

    добавил базовые переменные в класс Fraction, сложение дробей, явное приведение double в Fraction

--- Fraction.cs
+++ Fraction.cs
@@ -8,5 +8,105 @@ namespace HomeWorkCs4_2
 {
     internal class Fraction
     {
+        public int Numerator { get; set; }
+        private int denominator;
+        public int Denominator
+        {
+            get { 
+                return denominator;
+            }
+            set {
+                if (value == 0)
+                {
+                    denominator = 1;
+                }
+                else if (value < 0)
+                {
+                    denominator = value * (-1);
+                }
+                else { 
+                    denominator= value;
+                }
+
+            }
+        }
+        public Fraction(int num, int denom) {
+            Numerator = num;
+            Denominator= denom;
+            Reduction(this);
+        }
+        public Fraction(int num) : this(num, 1) { }
+        public Fraction() : this(0, 1) { }
+
+        public override string ToString()
+        {
+
+            return $"{Numerator}/{Denominator}";
+        }
+        //сумма дробей
+        public Fraction AddFraction(Fraction f) {
+            if (Denominator == f.Denominator)
+            {
+                return new Fraction(Numerator + f.Numerator, Denominator);
+            }
+            return new Fraction(Numerator * f.Denominator + f.Numerator * Denominator, Denominator * f.Denominator);
+        }
+        //переопределение оператора +
+        public static Fraction operator +(Fraction f1, Fraction f2) {
+            return f1.AddFraction(f2);
+        }
+        //разность дробей
+        //переопределение оператора -
+        //произведение дробей
+        //переопределение оператора *
+        //деление дробей
+        //переопределение оператора /
+        //переопределение метода Equals() 
+        //переопределение метода GetHashCode() 
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode(); 
+        }
+        //переопределение оператора ==
+        //переопределение оператора !=
+        //сокращение дроби
+        private void Reduction(Fraction f)
+        {
+
+            int n = f.MaxDiv();
+            if (n > 1)
+            {
+                f.Numerator /= n;
+                f.Denominator /= n;
+            }
+        }
+        //нахождение найбольшего общего делителя
+        private  int MaxDiv()
+        {
+            int n = Numerator;
+            int d = Denominator;
+            int temp;
+            while (d != 0)
+            {
+                temp = d;
+                d = n % d;
+                n = temp;
+            }
+            return n;
+        }
+        //создание Fraction из double
+        private static Fraction DoubleToFractin(double d) {
+            int n = 1;
+            double temp = d;
+            while (temp%1!= 0) {
+                n *= 10;
+                temp *= 10;
+            }
+            return new Fraction((int)(d*n), n);
+        }
+        //переопределение явного преобразования double в Fraction
+        public static explicit operator Fraction(Double d) {
+            return Fraction.DoubleToFractin(d);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace HomeWorkCs4_2
     {
         static void Main(string[] args)
         {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(2, 4);           
+            Fraction f3 = new Fraction(5, 50);           
+            Console.WriteLine($"f1 = {f1}");
+            Console.WriteLine($"f2 = {f2}");
+            Console.WriteLine($"f3 = {f3}");
+            Console.WriteLine($"f1+f2 = {f1+f2}");
+            Console.WriteLine($"1.5 = {(Fraction)1.55}");
+            
+
+            
         }
     }
 }
